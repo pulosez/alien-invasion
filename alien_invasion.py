b89f36a2ca71a0e17f7dab96ec90a42a1a61218a
pulosez <pulosez@gmail.com>
2021-08-13 14:41:39 +0300

    Added making fleet move

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -112,6 +113,24 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагує відповідно до того, чи досяг котрийсь із прибульців краю екрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Спуск всього флоту та зміна його напрямку."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """Перевірити, чи флот знаходиться на краю, тоді оновити позиції всіх прибульців флоту."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _update_screen(self):
         """Оновити зображення на екрані та перемкнутися на новий екран."""
